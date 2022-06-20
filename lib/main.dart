e418fc1dfa3e829c62174fe2e6ca6370c497467e
Rajat Vishwakarma <rajatvish6244444@gmail.com>
2022-06-20 16:27:38 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:great_places/providers/great_places.dart';
+import 'package:great_places/screens/add_places_screen.dart';
+import 'package:great_places/screens/places_list_screen.dart';
+import 'package:provider/provider.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider.value(
+      value: GreatPlaces(),
+      child: MaterialApp(
+        title: 'Great Places',
+        theme:
+            ThemeData(primarySwatch: Colors.indigo, accentColor: Colors.amber),
+        home: PlacesListScreen(),
+        routes: {
+          AddPlaceScreen.routeName: (ctx) => AddPlaceScreen(),
+        },
+      ),
+    );
+  }
+}
